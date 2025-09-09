74380b8128022755fa34a4f9f7739b8884401a42
HrEsben <esben@optus.dk>
2025-09-09 12:30:27 +0200

    feat: add React Query Devtools for enhanced debugging and development experience

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -3,6 +3,7 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import { ThemeProvider } from "next-themes"
 import { QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { system } from "./theme"
 import { SimpleToaster } from "./simple-toast"
 import { queryClient } from "@/lib/query-client"
@@ -21,6 +22,7 @@ export function Provider(props: { children: React.ReactNode }) {
           <SimpleToaster />
         </ThemeProvider>
       </ChakraProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
